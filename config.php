d549a3db7a6d082e2003b14519cfb294742274fb
chapu <chapupupupupu@yahoo.co.jp>
2020-01-04 12:19:50 +0900

    ブラウザにエラーを表示させる、エスケープ処理のためのconfig.php, functions.phpを作成。 index.phpに問題をシャッフルさせて表示させるコードを追加。

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+
+// エラー表示の設定
+ini_set('display_errors', 1);
+
+// エスケープのための関数を定義しているファイルを読み込み
+require_once(__DIR__ . '/functions.php');
+
+?>
--- index.php
+++ index.php
@@ -1,3 +1,30 @@
+<?php
+
+require_once(__DIR__ . '/config.php');
+
+// 問題をセット
+$quizSet = [];
+$quizSet[] = [
+  'q' => 'What is A?',
+  'a' => ['A0', 'A1', 'A2', 'A3']
+];
+$quizSet[] = [
+  'q' => 'What is B?',
+  'a' => ['B0', 'B1', 'B2', 'B3']
+];
+$quizSet[] = [
+  'q' => 'What is C?',
+  'a' => ['C0', 'C1', 'C2', 'C3']
+];
+
+// 今が何問目か
+$current_num = 0;
+
+// 問題を$dataに代入し、答えの選択表示をシャッフル
+$data = $quizSet[$current_num];
+shuffle($data['a']);
+
+?>
 <!DOCTYPE html>
 <html lang="ja">
 <head>
@@ -7,12 +34,11 @@
 </head>
 <body>
   <div id="container">
-    <h1>Q. What is A?</h1>
+    <h1>Q. <?php echo h($data['q']); ?></h1>
     <ul>
-      <li class="answer">A0</li>
-      <li class="answer">A1</li>
-      <li class="answer">A2</li>
-      <li class="answer">A3</li>
+      <?php foreach ($data['a'] as $a) : ?>
+        <li class="answer"><?php echo h($a); ?></li>
+      <?php endforeach; ?>
     </ul>
     <div id="btn" class="disabled">Next Question</div>
   </div>
