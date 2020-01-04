9b5e079cb5dcaf5458659ce081b6b631614a4b64
chapu <chapupupupupu@yahoo.co.jp>
2020-01-04 15:08:55 +0900

    問題を作成するためのQuiz.phpを作成。

--- config.php
+++ config.php
@@ -3,6 +3,9 @@
 // エラー表示の設定
 ini_set('display_errors', 1);
 
+// sessionを使う
+session_start();
+
 // エスケープのための関数を定義しているファイルを読み込み
 require_once(__DIR__ . '/functions.php');
 
--- index.php
+++ index.php
@@ -1,27 +1,12 @@
 <?php
 
 require_once(__DIR__ . '/config.php');
+require_once(__DIR__ . '/Quiz.php');
 
-// 問題をセット
-$quizSet = [];
-$quizSet[] = [
-  'q' => 'What is A?',
-  'a' => ['A0', 'A1', 'A2', 'A3']
-];
-$quizSet[] = [
-  'q' => 'What is B?',
-  'a' => ['B0', 'B1', 'B2', 'B3']
-];
-$quizSet[] = [
-  'q' => 'What is C?',
-  'a' => ['C0', 'C1', 'C2', 'C3']
-];
+// Quizクラスからインスタンスを生成
+$quiz = new MyApp\Quiz();
 
-// 今が何問目か
-$current_num = 0;
-
-// 問題を$dataに代入し、答えの選択表示をシャッフル
-$data = $quizSet[$current_num];
+$data = $quiz->getCurrentQuiz();
 shuffle($data['a']);
 
 ?>
